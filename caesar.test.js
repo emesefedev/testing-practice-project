96a8181ba73aa8accd824b313818a7ec3fcb86f2
emesefedev <emesefedev@gmail.com>
2024-12-12 16:26:13 +0100

    test: caesarCipher describe

--- caesar.test.js
+++ caesar.test.js
@@ -1,13 +1,16 @@
 import { caesarCipher } from "./caesar.js"
 
-test.skip("Caesar Cipher for xyz with step 3", () => {
-    expect(caesarCipher("xyz", 3)).toBe("abc")
+describe.skip("The divide() function", () => {
+    test("Caesar Cipher for xyz with step 3", () => {
+        expect(caesarCipher("xyz", 3)).toBe("abc")
+    })
+    
+    testg("Caesar Cipher with case preservation", () => {
+        expect(caesarCipher("Xyz", 3)).toBe("Abc")
+    })
+    
+    test("Caesar Cipher with punctuation", () => {
+        expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!")
+    })
 })
 
-test.skip("Caesar Cipher with case preservation", () => {
-    expect(caesarCipher("Xyz", 3)).toBe("Abc")
-})
-
-test("Caesar Cipher with punctuation", () => {
-    expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!")
-})
